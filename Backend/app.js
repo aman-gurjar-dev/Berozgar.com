6a0c00de0f07c47ebb111e238b9fff21f57ab464
aman-gurjar-dev <amangurjar160@gmail.com>
2025-06-21 19:15:55 +0530

    cors

--- Backend/app.js
+++ Backend/app.js
@@ -20,7 +20,6 @@ app.use(
     origin: process.env.CLIENT_URL,
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
